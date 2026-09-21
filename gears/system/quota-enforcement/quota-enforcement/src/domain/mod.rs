c90628d62eaa526a9af39f3a602a2744f4aaec16
Vladan Gojgic <v.gojgic@gmail.com>
2026-09-21 17:36:27 +0200

    feat(quota-enforcement): domain output ports and the metrics meter
    Signed-off-by: Vladan Gojgic <v.gojgic@gmail.com>

--- gears/system/quota-enforcement/quota-enforcement/src/domain/mod.rs
+++ gears/system/quota-enforcement/quota-enforcement/src/domain/mod.rs
@@ -1,5 +1,7 @@
 //! Domain layer of the quota-enforcement gear.
 
 pub mod error;
+pub mod ports;
 
 pub use error::{Dependency, DomainError, PluginKind, ResourceKind};
+pub use ports::{CoordinatorBinding, LeaderWork, SingletonCoordinator, SingletonScope};
