7ec3f5a703041e22284024a9127e8d6910e5790a
Vladan Gojgic <v.gojgic@gmail.com>
2026-09-21 17:37:24 +0200

    feat(quota-enforcement): plugin resolution, admission and the enforcement point
    Signed-off-by: Vladan Gojgic <v.gojgic@gmail.com>

--- gears/system/quota-enforcement/quota-enforcement/src/domain/mod.rs
+++ gears/system/quota-enforcement/quota-enforcement/src/domain/mod.rs
@@ -1,7 +1,12 @@
 //! Domain layer of the quota-enforcement gear.
 
+pub mod admission;
 pub mod error;
+pub mod pep;
+pub mod plugins;
 pub mod ports;
 
+pub use admission::{Admission, AdmissionTarget, Admitted};
 pub use error::{Dependency, DomainError, PluginKind, ResourceKind};
+pub use plugins::PluginBinding;
 pub use ports::{CoordinatorBinding, LeaderWork, SingletonCoordinator, SingletonScope};
